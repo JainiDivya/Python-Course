db84361ba992596f51bb05384fdd0b605d7a0467
divyajaini789 <jaini.divya123@gmail.com>
2026-03-11 22:18:34 +0530

    average of 5 numbers

---
+++ Data Types in python/average.py
@@ -0,0 +1,11 @@
+Tree1 = 24
+Tree2 = 35
+Tree3 = 40
+Tree4 = 60
+Tree5 = 56
+
+sum = Tree1+Tree2+Tree3+Tree4+Tree5
+print("sum of all 5 trees", sum)
+
+Average = sum/5
+print("Average of all 5 trees", Average)

a9ac9f75120f9b844132ef376e00a3fa9929bf09
divyajaini789 <jaini.divya123@gmail.com>
2026-03-12 22:24:25 +0530

    exam eligibilty

--- Lesson 7 Nested conditional statements/exam_eligibility_check.py
+++ Lesson 7 Nested conditional statements/exam_eligibility_check.py
@@ -4,7 +4,7 @@ if Medical_cause == "Yes":
     print("You are allowed for exam")
 else:
   atten = int(input("Enter the attendance of the student"))
-
+  
 if atten>=75:
      print("You are allowe dfor exam")
 else:

2fb5f9846db8ffcf5d37365c1a029c1c2328f81c
divyajaini789 <jaini.divya123@gmail.com>
2026-03-12 22:07:18 +0530

    not equal operator

---
+++ Lesson 6 Python operators 2/Not_equal_operator.py
@@ -0,0 +1,11 @@
+a = 5
+b = 10
+c = 24
+
+print(a!=b)
+print(b!=c)
+
+a = "Coding"
+b = "Python"
+if a!=b:
+    print("a and b both are different")
---
+++ Lesson 6 Python operators 2/and_or_operator.py
@@ -0,0 +1,17 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("all Boolean values has True")
+else:
+    print("Atleast one boolean value has a value False")
+
+a = 4
+b = 5
+c = 13
+
+if a>b or b>c:
+    print("All bolean values has a value True")
+else:
+    print("Atleast one Boolenan value has a value False")

249a99bc9bdbb9e7338a1f81b50d73c67d09ca42
divyajaini789 <jaini.divya123@gmail.com>
2026-04-15 18:57:36 +0530

    reversed a string

---
+++ Lesson 8 Loops/Reverse_a_string.py
@@ -0,0 +1,8 @@
+string= input("Enetr a string that to be reversed:")
+string2=('')
+
+for i in string:
+    string2=i+string2
+
+print("The original String=", string)
+print("The reveresed string=", string2)
